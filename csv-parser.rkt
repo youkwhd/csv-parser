ea0cdc5470154b62b94d8d1d6b4fcc717850721e
youkwhd <lolywk@tutanota.com>
2023-08-28 00:10:12 +0700

    feat: read and parse csv

--- csv-parser.rkt
+++ csv-parser.rkt
@@ -2,18 +2,54 @@
 
 (require "io.rkt")
 
-(: parse-csv-header (-> String (HashTable String (Listof String))))
-(define (parse-csv-header csv)
-  (let ([ht : (HashTable String (Listof String)) (make-hash)]
-        [each-line : (Listof String) (string-split csv "\n" #:trim? #f)])
-    (for-each 
-      (lambda ([field : String])
-        (hash-set! ht field '()))
-      (string-split (list-ref each-line 0) "," #:trim? #f))
-    ht))
+(struct csv-metadata ([body : (HashTable String (Listof String))]
+                      [header : (Listof String)]
+                      [n-header-line : Integer])
+        #:transparent)
+
+(: parse-csv-header (-> (Listof String) csv-metadata))
+(define (parse-csv-header each-csv-line)
+  (let ([ht : (HashTable String (Listof String)) (make-hash)])
+    (csv-metadata 
+      ht 
+      (foldr
+        (lambda ([field : String]
+                 [fields : (Listof String)])
+          (hash-set! ht field '())
+          (cons field fields))
+        '()
+        (string-split (list-ref each-csv-line 0) "," #:trim? #f))
+      1)))
+
+(: parse-csv-body-line (-> String csv-metadata Void))
+(define (parse-csv-body-line csv-body-line metadata)
+  (let* ([values : (Listof String) (string-split csv-body-line ",")]
+         [values-length : Integer (length values)])
+    (let loop ([n : Integer 0]
+               [values : (Listof String) values])
+      (cond
+        [(= n values-length) (void)]
+        [else
+          (hash-set! 
+            (csv-metadata-body metadata) 
+            (list-ref (csv-metadata-header metadata) n) 
+            (append 
+              (hash-ref (csv-metadata-body metadata) (list-ref (csv-metadata-header metadata) n))
+              (list (first values))))
+          (loop (+ n 1) (rest values))]))))
+
+(: parse-csv-body (-> (Listof String) csv-metadata Void))
+(define (parse-csv-body csv-body-lines metadata)
+  (for-each 
+    (lambda ([line : String]) (parse-csv-body-line line metadata))
+    csv-body-lines)
+  (void))
 
 (: read-csv (-> Path-String (HashTable String (Listof String))))
 (define (read-csv path)
-  (parse-csv-header (read-file path)))
+  (let* ([each-line : (Listof String) (string-split (read-file path) "\n")]
+         [metadata : csv-metadata (parse-csv-header each-line)])
+    (parse-csv-body (list-tail each-line (csv-metadata-n-header-line metadata)) metadata)
+    (csv-metadata-body metadata)))
 
-(displayln (read-csv "samples/food.csv"))
+(read-csv "samples/food.csv")
