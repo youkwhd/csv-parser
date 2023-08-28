1d2515bfa83713c7c707b66e6b81207577596e44
youkwhd <lolywk@tutanota.com>
2023-08-28 19:01:27 +0700

    refactor: start from scratch

--- csv-parser.rkt
+++ csv-parser.rkt
@@ -2,51 +2,53 @@
 
 (require "io.rkt")
 
-(: csv-split (-> String Char (Listof String)))
-(define (csv-split str sep)
-  (let split-all ([chars : (Listof Char) (string->list str)])
-    (: result (Pairof String Integer))
-    (define result
-      (let split-until-sep ([chars : (Listof Char) chars]
-                            [buffer : String ""]
-                            [iteration : Integer 0]
-                            [ignore-sep? : Boolean #f])
-        (cond
-          [(empty? chars) (cons buffer iteration)]
-          [(and (not ignore-sep?) (equal? (first chars) sep)) (cons buffer (+ iteration 1))]
-          [(equal? (first chars) #\newline) (split-until-sep  (rest chars) buffer (+ iteration 1) ignore-sep?)]
-          [(equal? (first chars) #\") (split-until-sep (rest chars) buffer (+ iteration 1) (not ignore-sep?))]
-          [else
-            (split-until-sep (rest chars) (string-append buffer (string (first chars))) (+ iteration 1) ignore-sep?)])))
+(: boolean->integer (-> Boolean Integer))
+(define (boolean->integer bool)
+  (if bool 1 0))
+
+(: parse-line (-> String (Listof String)))
+(define (parse-line csv)
+  '())
+
+(: string-empty? (-> String Boolean))
+(define (string-empty? str)
+  (equal? (string-length str) 0))
+
+(: string-find (-> String String Integer))
+(define (string-find str target)
+  (let loop ([str : String str]
+             [index : Integer 0])
     (cond
-      [(empty? chars) '()]
+      [(string-empty? str) -1]
+      [(string=? (substring str 0 (string-length target)) target) index]
+      [else (loop (substring str 1) (+ index 1))])))
+
+(: string-find-either (-> String String String Integer))
+(define (string-find-either str t1 t2)
+  (let loop ([str : String str]
+             [index : Integer 0])
+    (cond
+      [(string-empty? str) -1]
+      [(string=? (substring str 0 (string-length t1)) t1) index]
+      [(string=? (substring str 0 (string-length t2)) t2) index]
+      [else (loop (substring str 1) (+ index 1))])))
+
+(: quote-wrapped? (-> String Boolean))
+(define (quote-wrapped? str)
+  #t)
+
+(: parse-csv (-> String (Listof String)))
+(define (parse-csv csv)
+  (let loop ([csv : String csv])
+    (define quote-wrapped? (string-prefix? csv "\""))
+    (define index-of-delim (if quote-wrapped? (string-find csv "\",") (string-find-either csv "," "\n")))
+    (cond
+      [(equal? index-of-delim -1) '()]
       [else
-        (cons (car result) (split-all (list-tail chars (cdr result))))])))
+        ;; (displayln (substring csv (+ 0 (boolean->integer quote-wrapped?)) index-of-delim))
+        ;; (displayln "")
+        (cons (substring csv (+ 0 (boolean->integer quote-wrapped?)) index-of-delim) (loop (substring csv (+ (+ index-of-delim 1) (boolean->integer quote-wrapped?)))))])))
 
-(: csv-header-split? (-> String Boolean))
-(define (csv-header-split? header)
-  (and 
-    (equal? (string-ref header 0) #\")
-    (not (equal? (string-ref header (- (string-length header) 1)) #\"))))
-
-;; (: parse-csv-header (-> (Listof String) (Pair Integer (Listof String))))
-(: parse-csv-header (-> (Listof String) (Listof String)))
-(define (parse-csv-header lines)
-  ;; (: parsed-header (Listof String))
-  ;; (define parsed-header 
-    (let combine-header ([headers : (Listof String) (string-split (first lines) "," #:trim? #f)]
-                         [traversed : Integer 1]
-                         [parsed? : Boolean #f])
-      (cond
-        [parsed? '()]
-        [(equal? (string-ref (first headers) 1) #\") '()]
-        [else
-          (cons (first headers) (combine-header (rest headers) 1 #f))])))
-
-(: read-csv (-> String (Listof (Listof String))))
-(define (read-csv path)
-  (let ([lines : (Listof String) (string-split path "\n")])
-    (displayln (parse-csv-header lines))
-    '()))
-
-;; (read-csv (read-file "currency.csv"))
+;; (parse-csv (read-file "currency.csv"))
+(parse-csv (read-file "samples/food.csv"))
+;; (parse-csv (read-file "color.csv"))
