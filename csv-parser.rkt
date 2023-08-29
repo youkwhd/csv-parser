2b2d9e98ca24ff405188178698cf1249eb47f314
youkwhd <lolywk@tutanota.com>
2023-08-29 19:48:08 +0700

    feat: add optional argument delimiter

--- csv-parser.rkt
+++ csv-parser.rkt
@@ -2,8 +2,8 @@
 
 (require "string.rkt")
 
-(: parse-field (-> String (Pairof (Pairof String String) Boolean)))
-(define (parse-field str)
+(: parse-field (-> String String (Pairof (Pairof String String) Boolean)))
+(define (parse-field str delimiter)
   (: field-metadata (Pairof Integer Boolean))
   (define field-metadata
     (let field-metadata ([nth : Integer 0]
@@ -12,7 +12,7 @@
       (cond
         [(string-empty? str) (cons nth #t)]
         [(and (string-prefix? str "\n") (not inside-quote?)) (cons nth #t)]
-        [(and (string-prefix? str ",") (not inside-quote?)) (cons nth #f)]
+        [(and (string-prefix? str delimiter) (not inside-quote?)) (cons nth #f)]
         [(string-prefix? str "\\\"") (field-metadata (+ nth 2) inside-quote? (substring str 2))]
         [(string-prefix? str "\"") (field-metadata (+ nth 1) (not inside-quote?) (substring str 1))]
         [else (field-metadata (+ nth 1) inside-quote? (substring str 1))])))
@@ -24,8 +24,8 @@
                            (+ (car field-metadata) 1))))
     (cdr field-metadata)))
 
-(: parse-csv (-> String (Listof (Listof String))))
-(define (parse-csv str)
+(: parse-csv (->* [String] [#:delimiter Char] (Listof (Listof String))))
+(define (parse-csv str #:delimiter [delimiter : Char #\,])
   (cond
     [(string-empty? str) '()]
     [else
@@ -34,13 +34,13 @@
         (let parse-line ([str : String str]
                          [fields : (Listof String) '()])
           (: field (Pairof (Pairof String String) Boolean))
-          (define field (parse-field str))
+          (define field (parse-field str (string delimiter)))
 
           (cond
             [(cdr field)
              (cons (append fields (list (remove-quote-capsulated (caar field)))) (cdar field))]
             [else
               (parse-line (cdar field) (append fields (list (remove-quote-capsulated (caar field)))))])))
-      (cons (car parsed-fields) (parse-csv (cdr parsed-fields)))]))
+      (cons (car parsed-fields) (parse-csv (cdr parsed-fields) #:delimiter delimiter))]))
 
 (provide (all-defined-out))
