e71fc1701234a547d426c43b5a30d048a534d99f
youkwhd <lolywk@tutanota.com>
2023-08-29 17:10:06 +0700

    refactor: move out let bindings to be independent

--- csv-parser.rkt
+++ csv-parser.rkt
@@ -3,6 +3,28 @@
 (require "io.rkt")
 (require "string.rkt")
 
+(: parse-field (-> String (Pairof (Pairof String String) Boolean)))
+(define (parse-field str)
+  (: field-metadata (Pairof Integer Boolean))
+  (define field-metadata
+    (let field-metadata ([nth : Integer 0]
+                         [inside-quote? : Boolean #f]
+                         [str : String str])
+      (cond
+        [(string-empty? str) (cons nth #t)]
+        [(and (string-prefix? str "\n") (not inside-quote?)) (cons nth #t)]
+        [(and (string-prefix? str ",") (not inside-quote?)) (cons nth #f)]
+        [(string-prefix? str "\\\"") (field-metadata (+ nth 2) inside-quote? (substring str 2))]
+        [(string-prefix? str "\"") (field-metadata (+ nth 1) (not inside-quote?) (substring str 1))]
+        [else (field-metadata (+ nth 1) inside-quote? (substring str 1))])))
+
+  (cons
+    (cons (substring str 0 (car field-metadata)) 
+          (substring str (if (> (+ (car field-metadata) 1) (string-length str))
+                           (string-length str)
+                           (+ (car field-metadata) 1))))
+    (cdr field-metadata)))
+
 (: f (-> String (Listof (Listof String))))
 (define (f str)
   (cond
@@ -11,33 +33,14 @@
       (: zz (Pairof (Listof String) String))
       (define zz (let parse-line ([str : String str]
                                   [fields : (Listof String) '()])
-        (: field-length (Pairof Integer Boolean))
-        (define field-length (let find-length ([nth : Integer 0]
-                                               [inside-quote? : Boolean #f]
-                                               [str : String str])
-                               (cond
-                                 [(string-empty? str) (cons nth #t)]
-                                 [(and (string-prefix? str "\n") (not inside-quote?)) (cons nth #t)]
-                                 [(and (string-prefix? str ",") (not inside-quote?)) (cons nth #f)]
-                                 [(string-prefix? str "\\\"") (find-length (+ nth 2) inside-quote? (substring str 2))]
-                                 [(string-prefix? str "\"") (find-length (+ nth 1) (not inside-quote?) (substring str 1))]
-                                 [else (find-length (+ nth 1) inside-quote? (substring str 1))])))
-        (: b (Pairof String String))
-        (define b
-          (let parse-field ([str : String str]
-                            [field : String ""])
-            (cond
-              [(string-empty? str) (cons field "")]
-              [else
-                (cons (substring str 0 (car field-length)) (substring str (if (> (+ (car field-length) 1) (string-length str))
-                                              (string-length str)
-                                              (+ (car field-length) 1))))])))
-
+        (: field (Pairof (Pairof String String) Boolean))
+        (define field (parse-field str))
+        
         (cond
-          [(cdr field-length)
-           (cons (append fields (list (car b))) (cdr b))]
+          [(cdr field)
+           (cons (append fields (list (caar field))) (cdar field))]
           [else
-            (parse-line (cdr b) (append fields (list (car b))))])))
+            (parse-line (cdar field) (append fields (list (caar field))))])))
       (cons (car zz) (f (cdr zz)))]))
 
 (: remove-quote-capsulated (-> String String))
