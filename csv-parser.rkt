d057ac616f525f086b137cf3fc9783e58d2a06ee
youkwhd <lolywk@tutanota.com>
2023-08-29 16:57:17 +0700

    fix: check if length is gt 2

--- csv-parser.rkt
+++ csv-parser.rkt
@@ -42,7 +42,7 @@
 
 (: remove-quote-capsulated (-> String String))
 (define (remove-quote-capsulated str)
-  (if (and (string-prefix? str "\"") (string-suffix? str "\""))
+  (if (and (>= (string-length str) 2) (string-prefix? str "\"") (string-suffix? str "\""))
     (substring str 1 (- (string-length str) 1))
     str))
 
